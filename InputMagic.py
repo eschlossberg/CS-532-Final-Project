2889083fcdaaa135569914d709db9c74224b812e
dannysj <csji4202@hotmail.com>
2017-12-01 14:02:04 -0600

    Fixed some minor issues on companies.json + importing json

--- InputMagic.py
+++ InputMagic.py
@@ -2,3 +2,15 @@
 from gensim import utils
 from gensim.models.doc2vec import LabeledSentence
 from gensim.models import Doc2Vec
+
+# numpy, etc
+import numpy
+import json
+
+## For Debug purpose
+from pprint import pprint
+
+## Cleaning the datas
+data = json.load(open('companies.json'))
+
+pprint(data)
