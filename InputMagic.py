71cdddba79183c6d0432c4cdb0b22ab51c48c55b
dannysj <csji4202@hotmail.com>
2017-12-02 22:48:19 -0600

    Bug fixes

--- InputMagic.py
+++ InputMagic.py
@@ -20,16 +20,29 @@ print(len(data['companies']))
 
 # Company - CleanArticles pair
 company_arr = []
+# Clean_articles group format:
+#  - Company
+#  |-- Article
+#  |---Sentences
 clean_articles = []
 
 for company in data['companies']:
     print("Company name: {}  with  {} articles".format(company['company'], len(company['articles'])))
     ca = []
     for article in company['articles']:
-        cleanString = re.sub('\W+', ' ', article['text'])
-        ca.append(cleanString)
-
+        test = re.split('[.]', article['text'])
+        sentences = []
+        for text in test:
+            if not text:
+                continue
+            cleanString = re.sub('\W+', ' ', text)
+            if not cleanString:
+                continue
+            sentences.append(cleanString)
+        print(sentences)
+        ca.append(sentences)
     company_arr.append(company['company'])
     clean_articles.append(ca)
 
+print(clean_articles)
 # Gensim model
