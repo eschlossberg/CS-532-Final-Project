afa2880facc45ac7dd4833ee711eaf28961ea783
dannysj <csji4202@hotmail.com>
2017-12-03 08:19:01 -0600

    Updated magic

--- InputMagic.py
+++ InputMagic.py
@@ -1,7 +1,7 @@
 # gensim modules
 from gensim import utils
 from gensim.models.doc2vec import LabeledSentence
-from gensim.models import Doc2Vec
+from gensim.models.doc2vec import d2v
 
 # numpy, etc
 import numpy
@@ -30,21 +30,59 @@ for company in data['companies']:
     print("Company name: {}  with  {} articles".format(company['company'], len(company['articles'])))
     ca = []
     for article in company['articles']:
-        test = re.split('[.]', article['text'])
+        raws = re.split('[.]', article['text'])
         sentences = []
-        for text in test:
-            if not text:
+        for raw in raws:
+            if not raw:
                 continue
-            cleanString = re.sub('\W+', ' ', text)
-            if not cleanString:
+            cleanRaw = re.sub('\W+', ' ', raw)
+            if not cleanRaw:
                 continue
-            sentences.append(cleanString)
+            sentences.append(cleanRaw.lower())
         print(sentences)
         ca.append(sentences)
     company_arr.append(company['company'])
     clean_articles.append(ca)
 
+# Copied code, for testing
+class LabeledLineSentence(object):
+    def __init__(self, sources):
+        self.sources = sources
+
+        flipped = {}
+
+        # make sure that keys are unique
+        for key, value in sources.items():
+            if value not in flipped:
+                flipped[value] = [key]
+            else:
+                raise Exception('Non-unique keys detected')
+
+    def __iter__(self):
+        for source, prefix in self.sources.items():
+            with utils.smart_open(source) as fin:
+                for item_no, line in enumerate(fin):
+                    yield LabeledSentence(utils.to_unicode(line).split(), [prefix + '_%s' % item_no])
+
+    def to_array(self):
+        self.sentences = []
+        for source, prefix in self.sources.items():
+            with utils.smart_open(source) as fin:
+                for item_no, line in enumerate(fin):
+                    self.sentences.append(LabeledSentence(utils.to_unicode(line).split(), [prefix + '_%s' % item_no]))
+        return self.sentences
+
+    def shuffle(self):
+        shuffle(self.sentences)
+        return self.sentences
+
 # Gensim model
+def generate_doc2vec_model(doc, lang_tag):
+    doc = LabeledLineSentence(sources)
+    for line in doc:
+        print(line) #Debug
+    model = d2v(doc, size=300, window=10, min_count=1, workers=8)
+    model.save('model.'+lang_tag)
 
 
 model = Doc2Vec(min_count=1, window=10, size=100, sample=1e-4, negative=5, workers=8)
