a9c68b1e90dc87c9dab1ffd5da3f6bcbe957e555
dannysj <csji4202@hotmail.com>
2017-12-02 22:21:37 -0600

    Added cleaning magic

--- InputMagic.py
+++ InputMagic.py
@@ -7,10 +7,19 @@ from gensim.models import Doc2Vec
 import numpy
 import json
 
-## For Debug purpose
+# For Debug purpose
 from pprint import pprint
+
+# For cleaning texts
+import re
 
 ## Cleaning the datas
 data = json.load(open('companies.json'))
 
 pprint(data)
+
+for company in data['companies']:
+    print("Cleaned string:")
+    for article in company['articles']:
+        cleanString = re.sub('\W+', ' ', article['text'])
+        print(cleanString)
