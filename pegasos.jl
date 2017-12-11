232cab94771f7461bbab3086ddd87d236a877f56
mcgrath <mcgrath@cs.wisc.edu>
2017-12-10 20:18:23 -0600

    tested pegasos against MNIST for sanity

--- pegasos.jl
+++ pegasos.jl
@@ -5,7 +5,7 @@ function pegasos(supports::Array{Float64},y::Array{Float64} ,lamb::Float64, max_
 	for K = 1:max_iter
 		#support vector to increment by
 		idx = sample(collect(1:size(supports,2)))
-		new_alpha = zeros(size(suppors,2))
+		new_alpha = zeros(size(supports,2))
 		filter_idxs = filter( x-> x != idx,collect(1:size(alpha,1)))
 		new_alpha[filter_idxs] = alpha[filter_idxs]
 		#handle the idx case
@@ -26,10 +26,10 @@ function pegasos(supports::Array{Float64},y::Array{Float64} ,lamb::Float64, max_
 end
 
 #phi is just the transformation into a higher dimensional space
-function alpha_to_w(alpha::Array{Float64}, lamb::Float64, t::Int64, supports::Array{float64},y::Array{Float64},phi )
+function alpha_to_w(alpha::Array{Float64}, lamb::Float64, t::Int64, supports::Array{Float64},y::Array{Float64},phi )
 	sum_part = 0
 	for i =1:size(supports,2)
-		sum_part +=alpha[i]*y[i]*phi(supports[:,i])
+		sum_part +=alpha[i]*y[i]*phi(supports[i,:])
 	end
 	return (1/(lamb*t))*sum_part
 end
