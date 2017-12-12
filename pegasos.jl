965aae2aea77e5030ac3a3396433284e8130b1b7
mcgrath <mcgrath@cs.wisc.edu>
2017-12-12 17:55:33 -0600

    performance improvements for pegasos

--- pegasos.jl
+++ pegasos.jl
@@ -1,18 +1,19 @@
 using StatsBase
 
 function pegasos(supports::Array{Float64},y::Array{Float64} ,lamb::Float64, max_iter::Int64, kernel)
-	alpha = zeros(size(supports,2))
-	for K = 1:max_iter
+	alpha = SharedArray{Float64}(size(supports,1))
+	alpha[:] = 0
+	@parallel for K = 1:max_iter
 		#support vector to increment by
-		idx = sample(collect(1:size(supports,2)))
-		new_alpha = zeros(size(supports,2))
+		idx = sample(collect(1:size(supports,1)))
+		new_alpha = zeros(size(supports,1))
 		filter_idxs = filter( x-> x != idx,collect(1:size(alpha,1)))
 		new_alpha[filter_idxs] = alpha[filter_idxs]
 		#handle the idx case
-		test_val = y[idx]*(lamb*K)^(-1)
+		test_val = y[idx]*((lamb*K)^(-1))
 		sum_part = 0
-		for i = 1:size(supports,2)
-			sum_part += alpha[i]*y[idx]*kernel(supports[:,idx], supports[:,i])
+		for i = 1:size(supports,1)
+			sum_part += alpha[i]*y[idx]*kernel(supports[idx,:], supports[i,:])
 		end
 		test_val *= sum_part
 		if test_val < 1
@@ -22,14 +23,30 @@ function pegasos(supports::Array{Float64},y::Array{Float64} ,lamb::Float64, max_
 		end
 		alpha = new_alpha
 	end
-	return alpha
+	return Array(alpha)
 end
 
-#phi is just the transformation into a higher dimensional space
-function alpha_to_w(alpha::Array{Float64}, lamb::Float64, t::Int64, supports::Array{Float64},y::Array{Float64},phi )
-	sum_part = 0
-	for i =1:size(supports,2)
-		sum_part +=alpha[i]*y[i]*phi(supports[i,:])
+#non kernel version of pegasos
+function pegasos(supports::Array{Float64}, y::Array{Float64}, lamb::Float64, T::Int64)
+	w = zeros(size(supports,2))
+	for t = 1:T
+		idx = sample(collect(1:size(supports,1)))
+		nt = 1/(lamb*t)
+		if y[idx]*dot(w, supports[idx,:]) < 1
+			w = (1 - nt *lamb)*w + nt*y[idx]* supports[idx,:]
+		else
+			w = (1 - nt *lamb)*w
+		end
 	end
-	return (1/(lamb*t))*sum_part
+	return w
+end
+
+function predict(alpha::Array{Float64}, kernel, x::Array{Float64}, supports::Array{Float64}, y::Array{Float64})
+	pred_sub = SharedArray{Float64}(size(supports,1))
+	pred_sub[:] = 0
+	to_check = filter(x-> alpha[x] != 0, 1:size(alpha,1))
+	for i in to_check
+		pred_sub[i] =  alpha[i]*kernel(x, supports[i,:])*y[i]
+	end
+	return sign(sum(pred_sub))
 end
---
+++ pegasos_test/test.jl
@@ -0,0 +1,69 @@
+include("../pegasos.jl")
+using MLDatasets
+addprocs(3)
+train_data, train_labels = MNIST.traindata()
+#build training vectors
+println("building training vectors ...")
+train_data = reshape(train_data[:,:,1:10000], 10000, 784)
+#build the label vector
+#create indices for recoginizing 0
+train_labels = train_labels[1:10000]
+zero_indices = filter(x -> train_labels[x] == 0, 1:size(train_labels, 1))
+y_filtered = zeros(10000, 1)
+y_filtered[:] = -1.0
+y_filtered[zero_indices] = 1.0
+
+function error_rate(test, real)
+    trials = size(test,1)
+    ninc = 0
+    for i in 1:trials
+        if test[i] != real[i]
+            ninc +=1
+        end
+    end
+    return ninc/trials
+end
+
+X = train_data
+y = y_filtered
+lamb = 0.5
+
+w = (X'*X + lamb *eye(784))^(-1)*X'*y
+#now use w to make preds
+println("building test set ...")
+pred_vecs, test_labels = MNIST.testdata()
+pred_vecs = reshape(pred_vecs,10000 ,784 )
+
+real_ans = test_labels
+zero_indices = filter(x->real_ans[x] == 0, 1:size(test_labels,1))
+real_ans[:] = -1.0
+real_ans[zero_indices] = 1.0
+
+preds = sign.(pred_vecs*w)
+@printf "Thikonov Error Rate on MNIST: %f\n" error_rate(preds, real_ans)
+
+#define a kernel function
+function RBF(x::Array{Float64}, y::Array{Float64})
+    return exp(-norm(x - y)*.02)
+end
+
+function polynomial(x::Array{Float64}, y::Array{Float64})
+	return (dot(x,y) + 1)^2
+end
+
+alpha = pegasos(X,y, 0.005, 4000, RBF)
+alpha_poly = pegasos(X, y, 0.05, 2000, polynomial)
+w_2 = pegasos(X,y, 0.1,500 )
+println("making predictions")
+preds = SharedArray{Float64}(size(pred_vecs,1))
+@parallel for idx in 1:size(pred_vecs, 1)
+	preds[idx] = predict(alpha, RBF, pred_vecs[idx,:], X,y)
+end
+@printf "Pegasos Error Rate with RBF on MNIST: %f \n" error_rate(preds, real_ans)
+preds_poly = SharedArray{Float64}(size(pred_vecs,1))
+@parallel for i in 1:size(pred_vecs,1)
+	preds_poly[i] = predict(alpha_poly, polynomial, pred_vecs[i,:], X,y)
+end
+@printf "Pegasos Polynomial Kernel: %f \n" error_rate(preds_poly, real_ans)
+@printf "Pegasos No Kernel: %f \n" error_rate(sign.(pred_vecs*w_2), real_ans)
+@printf "Vector of all ones %f\n" error_rate(ones(size(real_ans,1)), real_ans)
